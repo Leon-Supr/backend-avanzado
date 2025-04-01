4ddf753af9b4d6cd12e97dfb9d640837c084d69b
Leonardo Arguello <Leon-Supr@users.noreply.github.com>
2025-03-31 20:26:47 -0600

    Add hashing method for user password

--- src/models/User.js
+++ src/models/User.js
@@ -1,19 +1,30 @@
 import mongoose from "mongoose"; // Porque va a interpretar a partir de un objeto MongoDB
 
 const UserSchema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
         require: true,
         trim: true,
         maxlength: 50
     },
-    email:{
+    email: {
         type: String,
         require: true,
         unique: true,
         match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     },
+    password: {
+        type: String,
+        require: true
+    }
 })
+
+// Método para hashear password antes de guardar el usuario
+UserSchema.pre('save', async (next) => {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12)
+    next()
+}) //pre es para hacer antes de una acción
 
 const User = mongoose.model('User', UserSchema)
 export default User
