8fcad1e10a92c69c766ffb4d786ce137b1281744
Leonardo Arguello <Leon-Supr@users.noreply.github.com>
2025-04-02 21:14:54 -0600

    Add validations middlewares with celebrate and joi

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-import dotenv from "dotenv"
 import express from "express" // Para creación del servidor backend
 
 import connectDB from "./src/config/db.js"
 import healthCheckRoutes from "./src/routes/healthCheckRoutes.js"
 import userRoutes from "./src/routes/userRoutes.js"
 import authRoutes from "./src/routes/authRoutes.js"
+import {errors} from "celebrate";
+
 
 connectDB()
 
@@ -15,6 +16,8 @@ app.use(express.json())
 app.use("/api/v0/", healthCheckRoutes)
 app.use("/api/v0/users", userRoutes)
 app.use("/api/v0/auth", authRoutes)
+
+app.use(errors())
 
 const PORT = 5001
 app.listen(PORT, ()=>{
