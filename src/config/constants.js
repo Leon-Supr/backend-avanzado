f2e87b4df1ba1c174f323bb0dac8d3abb53c2e87
Leonardo Arguello <Leon-Supr@users.noreply.github.com>
2025-04-09 19:45:29 -0600

    Fixed code, uploadLocal works now

--- src/config/constants.js
+++ src/config/constants.js
@@ -12,3 +12,8 @@ export const mongoUri = process.env.MONGO_URI
 // Firebase
 export const firebaseStorage = process.env.FIREBASE_STORAGE_BUCKET
 export const googleCredentials = process.env.GOOGLE_APPLICATION_CREDENTIALS
+
+export const projectId = process.env.GOOGLE_PROJECT_ID
+export const clientEmail = process.env.GOOGLE_CLIENT_EMAIL
+export const privateKey = process.env.PRIVATE_KEY?.replace(/\n/g, )
+export const googlestorageBucket = process.env.GOOGLE_STORAGE_BUCKET
